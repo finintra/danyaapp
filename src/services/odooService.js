400e0b1dc2fd1fe13e37c11556cd525dbf857d3c
arseniyfinberg <arseniyfinberg@users.noreply.github.com>
2025-11-06 12:32:38 +0200

    Fix: Correct axios timeout syntax in authenticate methods

--- src/services/odooService.js
+++ src/services/odooService.js
@@ -62,7 +62,6 @@ class OdooService {
       
       // Authenticate with Odoo
       const response = await axios.post(requestUrl, {
-        timeout: 30000, // 30 seconds timeout
         jsonrpc: '2.0',
         method: 'call',
         params: {
@@ -246,7 +245,6 @@ class OdooService {
       
       // Authenticate directly with Odoo session
       const response = await axios.post(`${this.url}/web/session/authenticate`, {
-        timeout: 30000, // 30 seconds timeout
         jsonrpc: '2.0',
         method: 'call',
         params: {
